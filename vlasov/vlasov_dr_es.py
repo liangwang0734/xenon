2f6a1f5720db0f7e75b7b60a11ca26c8a61aa58b
liangwang0734 <liang.wang.phys@gmail.com>
2023-04-06 13:54:47 -0400

    formatting

--- vlasov/vlasov_dr_es.py
+++ vlasov/vlasov_dr_es.py
@@ -1,4 +1,4 @@
-__all__ = ['k2w_es1d', 'k2w_es3d']
+__all__ = ["k2w_es1d", "k2w_es3d"]
 
 import numpy as np
 import scipy.linalg
@@ -7,10 +7,10 @@ from .vlasov_utils import bzs, czs
 import logging
 
 
-def k2w_es1d(ks, species, params, J=8, sort='real'):
+def k2w_es1d(ks, species, params, J=8, sort="real"):
     """Compute dispersion relation for the unmagnetized 1d Vlasov-Poisson
     system.
-  
+
     The basic algorithm follows [1]. The Z function is approximated by a J-pole
     Pade expansion and the bi-Maxwellian distribution is approximated by a
     truncated summation of Bessel functions. This way, a linear system is
@@ -18,7 +18,7 @@ def k2w_es1d(ks, species, params, J=8, sort='real'):
     equivalent matrix.
 
     [1]: https://iopscience.iop.org/article/10.1088/1009-0630/18/2/01/pdf
-  
+
     Args:
         ks (np.ndarray): An array of `k` values.
         species (np.ndarray of list): A list of parameters of each plasma
@@ -26,9 +26,9 @@ def k2w_es1d(ks, species, params, J=8, sort='real'):
             including `q, m, n0, v0, p0`. An example for a
             plasma with isothermal electrons and adiabatic:
 
-                species = np.array([  
-                    [-1,  1, 1, 0, 0.25],  # electron  
-                    [+1, 25, 1, 0, 0.81],  # ion  
+                species = np.array([
+                    [-1,  1, 1, 0, 0.25],  # electron
+                    [+1, 25, 1, 0, 0.81],  # ion
                 ])
 
         params (dict): A dictionary of relevant parameters. Here, the used ones
@@ -42,9 +42,9 @@ def k2w_es1d(ks, species, params, J=8, sort='real'):
             `kx` value.
     """
     q, m, n, v, p = np.rollaxis(np.array(species), axis=1)
-    eps0 = params['epsilon0']
-    vt = np.sqrt(2. * p / n / m)
-    lambdaD = np.sqrt(eps0 * p / (n * q)**2)
+    eps0 = params["epsilon0"]
+    vt = np.sqrt(2.0 * p / n / m)
+    lambdaD = np.sqrt(eps0 * p / (n * q) ** 2)
     S = len(q)
 
     bz, cz = bzs[J], czs[J]
@@ -53,7 +53,7 @@ def k2w_es1d(ks, species, params, J=8, sort='real'):
     m = 0
     for s in range(S):
         for j in range(J):
-            B[m] = bz[j] * cz[j] * vt[s] / (lambdaD[s]**2)
+            B[m] = bz[j] * cz[j] * vt[s] / (lambdaD[s] ** 2)
             C[m] = v[s] + cz[j] * vt[s]
             m += 1
 
@@ -67,13 +67,13 @@ def k2w_es1d(ks, species, params, J=8, sort='real'):
         w = scipy.linalg.eigvals(M)
 
         sort_idx = slice(None)
-        if sort in ['imag']:
+        if sort in ["imag"]:
             sort_idx = np.argsort(w.imag + 1j * w.real)
             sort_idx = sort_idx[::-1]
-        elif sort in ['real']:
+        elif sort in ["real"]:
             sort_idx = np.argsort(w)
-        elif sort != 'none':
-            raise ValueError('`sort` value {} not recognized'.format(sort))
+        elif sort != "none":
+            raise ValueError("`sort` value {} not recognized".format(sort))
         w = w[sort_idx]
 
         ws[ik, :] = w
@@ -82,24 +82,24 @@ def k2w_es1d(ks, species, params, J=8, sort='real'):
 
 
 def k2w_es3d(
-        kxs,
-        kzs,
-        species,
-        params,
-        isMag=None,
-        J=8,
-        N=10,
-        check_convergence=True,
-        convergence_thresh=0.975,
-        sort="real",
-        dry_run=False,
+    kxs,
+    kzs,
+    species,
+    params,
+    isMag=None,
+    J=8,
+    N=10,
+    check_convergence=True,
+    convergence_thresh=0.975,
+    sort="real",
+    dry_run=False,
 ):
     """
     Compute electrostatic dispersion relation for a magnetized Vlasov-Poisson
     system assuming the background magnetic field along `z` and the wavevector
     `k` can have components along `x` and `z`, i.e., the perpendicular and
     parallel directions.
-  
+
     The basic algorithm follows [1]. The Z function is approximated by a J-pole
     Pade expansion and the bi-Maxwellian distribution is approximated by a
     truncated summation of Bessel functions. This way, a linear system is
@@ -107,7 +107,7 @@ def k2w_es3d(
     equivalent matrix.
 
     [1]: https://iopscience.iop.org/article/10.1088/1009-0630/18/2/01/pdf
-  
+
     Args:
         kxs (np.ndarray): Wavevector component values along `x`.
         kzs (np.ndarray): Wavevector component values along `z`.
@@ -127,35 +127,35 @@ def k2w_es3d(
         convergence_thresh (float): See option `N`. Default is 0.975.
         sort (str): Order for sorting computed frequencies. Default is "real".
         sort (str): `'real'` or `'imag'` or `'none'`. Order to sort results.
-  
+
     Returns:
         ws (np.ndarray or list): Computed frequencies in a list. `ws[ik]` is the
           solution for the `ik`th wavevector. The result is packed as a
           `np.ndarray` if the `N` values used for all `k` are the same.
     """
     q, m, n, vx, vz, px, pz = np.rollaxis(np.array(species), axis=1)
-    eps0 = params['epsilon0']
-    Bz = params['Bz']
+    eps0 = params["epsilon0"]
+    Bz = params["Bz"]
 
     S = len(q)  # number of species
-    lamDz = np.sqrt(eps0 * pz / (n * q)**2)
-    vtz = np.sqrt(2. * pz / n / m)
-    vtx = np.sqrt(2. * px / n / m)
+    lamDz = np.sqrt(eps0 * pz / (n * q) ** 2)
+    vtz = np.sqrt(2.0 * pz / n / m)
+    vtx = np.sqrt(2.0 * px / n / m)
     wc = q * Bz / m
 
     if isMag is None:
         isMag = [True] * S
-    assert (len(isMag) == S)
+    assert len(isMag) == S
 
     bz, cz = bzs[J], czs[J]
 
-    Tz_Tx = (vtz / vtx)**2
+    Tz_Tx = (vtz / vtx) ** 2
     rc = vtx / np.sqrt(2) / np.abs(wc)
 
-    ks = np.sqrt(kzs**2 + kxs**2)
+    ks = np.sqrt(kzs ** 2 + kxs ** 2)
     nk = len(ks)
     ws = []
-    Ns = np.zeros((nk, ), dtype=np.int32)
+    Ns = np.zeros((nk,), dtype=np.int32)
 
     if check_convergence:
         if np.any(isMag):
@@ -163,9 +163,9 @@ def k2w_es3d(
                 Nmax = N
                 kz = kzs[ik]
                 kp = kxs[ik]
-                kp_rc2m = ((kp * rc[isMag])**2).max()
+                kp_rc2m = ((kp * rc[isMag]) ** 2).max()
                 found = False
-                while (not found):
+                while not found:
                     bessSum = ive(range(-Nmax, Nmax + 1), kp_rc2m).sum()
                     if bessSum < convergence_thresh:
                         Nmax = Nmax + 1
@@ -174,7 +174,9 @@ def k2w_es3d(
                         if ik == len(ks) - 1:
                             logging.debug(
                                 "(kp*rc)^2 {}, N {}, bessSum {}".format(
-                                    kp_rc2m, Nmax, bessSum))
+                                    kp_rc2m, Nmax, bessSum
+                                )
+                            )
                 Ns[ik] = Nmax
     if dry_run:
         print("Ns {}".format(Ns))
@@ -193,20 +195,33 @@ def k2w_es3d(
         for s in range(S):
             if isMag[s]:
                 for n in range(-N4k, N4k + 1):
-                    bessel = ive(n, (kp * rc[s])**2)  # modified Bessel function
+                    bessel = ive(
+                        n, (kp * rc[s]) ** 2
+                    )  # modified Bessel function
                     for j in range(J):
-                        B[m] = bessel * bz[j] \
-                            * (cz[j] * kz * vtz[s] + n * wc[s] * Tz_Tx[s]) \
-                            / (lamDz[s] * k)**2
-                        C[m] = kz * vz[s] + kp * vx[s] + n * wc[s] \
-                             + cz[j] * kz * vtz[s]
+                        B[m] = (
+                            bessel
+                            * bz[j]
+                            * (cz[j] * kz * vtz[s] + n * wc[s] * Tz_Tx[s])
+                            / (lamDz[s] * k) ** 2
+                        )
+                        C[m] = (
+                            kz * vz[s]
+                            + kp * vx[s]
+                            + n * wc[s]
+                            + cz[j] * kz * vtz[s]
+                        )
                         m += 1
             else:
-                bessel = 1. / (2. * N4k + 1.)
+                bessel = 1.0 / (2.0 * N4k + 1.0)
                 for n in range(-N4k, N4k + 1):
                     for j in range(J):
-                        B[m] = bessel * bz[j] * (cz[j] * k * vtz[s]) \
-                             / (lamDz[s] * k)**2
+                        B[m] = (
+                            bessel
+                            * bz[j]
+                            * (cz[j] * k * vtz[s])
+                            / (lamDz[s] * k) ** 2
+                        )
                         C[m] = kz * vz[s] + kp * vx[s] + cz[j] * k * vtz[s]
                         m += 1
 
@@ -221,8 +236,8 @@ def k2w_es3d(
             sort_idx = np.argsort(w.imag + 1j * w.real)
         elif sort in ["real"]:
             sort_idx = np.argsort(w)
-        elif sort != 'none':
-            raise ValueError('`sort` value {} not recognized'.format(sort))
+        elif sort != "none":
+            raise ValueError("`sort` value {} not recognized".format(sort))
         ws.append(w[sort_idx])
 
     if Ns.min() == Ns.max():
